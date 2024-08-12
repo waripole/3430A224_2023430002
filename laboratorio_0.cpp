565f07dcf8a5b01f9613280a71635afc5e831b56
waripole <iliketocookireallydo@gmail.com>
2024-08-11 21:02:26 -0400

    Se agregó el int main

---
+++ laboratorio_0.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &num);
+    printf("Se ingreso un %d", num);
+    
+    return 0;
+}
