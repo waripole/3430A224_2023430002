8c4b2ebd50971cf99f89326fbb6fd9f11be0217f
waripole <iliketocookireallydo@gmail.com>
2024-08-13 00:06:15 -0400

    Se agregaron las funciones es_par_ono y longitud_s
    NOTA: La función longitud_string no funciona correctamente aún.

--- laboratorio_0.cpp
+++ laboratorio_0.cpp
@@ -1,12 +1,55 @@
-//#include <iostream>
+#include <iostream>
+#include <string>
 #include <stdio.h>
+
+void es_par_ono(int num)
+{
+    if(num%2==0)
+    {
+        printf("El número es par");
+    }
+    else
+    {
+        printf("El número es impar");
+    }
+
+}
+
+//WRONG POR AHORA
+int longitud_string(const std::string& str)
+{
+    int longitud = 0;
+
+    //for(int i = valor inicial; i <= valor final; i = i + paso)
+    for(char c : str)
+    {
+        longitud = longitud + 1;
+    }
+
+    printf("La longitud de la palabra es: ");
+
+    return longitud;
+}
 
 int main()
 {
     int num;
     printf("Ingrese un numero entero: ");
     scanf("%d", &num);
-    printf("Se ingreso un %d", num);
+    //printf("Se ingreso un %d", num);
+
+    es_par_ono(num);
+
+    //imprimir lo mismo para los números de 1 al 10 - MASOMENOS
+    //for(int i = valor inicial; i <= valor final; i = i + paso)
+    for(int i=1;i<=10;i+=1)
+    {
+        es_par_ono(i);
+    }
     
+    char input[100]; //arreglo de caracteres
+    printf("Ingrese una cadena de texto: ");
+    scanf("%99s", input);
+
     return 0;
 }
