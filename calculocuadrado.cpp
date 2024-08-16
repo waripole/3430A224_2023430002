e3f230e4f35ceb77f366f1e246d0e72d530e7eaf
waripole <iliketocookireallydo@gmail.com>
2024-08-16 03:54:23 -0400

    Se ordenó un poco el código

--- calculocuadrado.cpp
+++ calculocuadrado.cpp
@@ -7,12 +7,12 @@ double square(double x) // square a double precision floating-point number
 
 void print_square(double x) // si es void no se usa el return 0, pues void significa que no devuelve un valor
 {
-    std::cout << "the square of " << x << " is " << square(x) << '\n';
+    std::cout << "El cuadrado de " << x << " es " << square(x) << '\n';
 }
 
 int main()
 {
-    print_square(1.234); // print: the square of 1.234 is 1.52276
-    print_square(5.555); // print: the square of 5.555 is 30.858
+    print_square(2);
+    print_square(108);
     return 0;    
 }
