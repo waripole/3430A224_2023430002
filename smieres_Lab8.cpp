1c1df8128a3780da0bdc2bfe5795554513a529fd
waripole <iliketocookireallydo@gmail.com>
2024-10-25 01:04:26 -0300

    Se agrega archivo con las funciones para quicksort y método de selección

--- smieres_Lab8.cpp
+++ smieres_Lab8.cpp
@@ -1,1 +1,175 @@
-//hola
+//lab 8 - Quicksort y Selecction sort
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+
+void llenarArreglo(int* arr, int size) {
+    // Llenar el arreglo con numeros aleatorios entre 1 y 1000
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 1000 + 1; // Genera un numero entre 1 y 1000
+    }
+}
+
+
+void mostrarArreglo(int* arr, int size) {
+    // Mostrar los valores del arreglo
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+// intercambiar los valoress de dos variables int con punteros
+void swap(int *xp, int *yp){
+    int temp = *xp; // guardar el valor apuntado en una variables temporal
+    *xp = *yp;		// asignar el valor apuntado por 'yp' al lugar apuntado por 'xp'
+    *yp = temp;		// asignar el valor apuntado por 'temp' al lugar apuntado por 'yp's
+}
+
+// Mètodo de Selecciòn---------------------------------------
+
+void metodoSeleccion(int arr[], int n){
+	int i;
+	int j;
+	int min_idx;
+
+	for(int i = 0; i < n-1; i++){
+
+		min_idx = i;
+
+		for(j = i+1; j < n; j++){
+
+			if(arr[j] < arr[min_idx]){
+				min_idx = j;
+			}			
+		}
+		swap(&arr[min_idx], &arr[i]);
+	}
+
+	/*
+	for(int i = 0; i < n; i++){
+		std::cout<<"MIAU-> "<< arr[i]<<std::endl;	
+	}
+	*/
+}
+
+//  Mètodo QuickSort-----------------------------------------
+
+/*funciòn para partir el arreglo de elementos adoc al pivote, 
+los elementos menores se van a la izquierda y los mayores a la derecha
+
+low -> corresponde a la posiciòn 0
+high -> corresponde a la ùltima posiciòn 
+*/
+int partir_arreglo (int arr[], int low, int high){
+    
+    int pivote = arr[high];
+    int i = (low - 1); // -> esto marca la ùltima posiciòn en donde se puso un elemento menor o igual al pivote
+
+    // recorrer el arreglo dessde la posiciòn 0 hasta 1 antes de la ùltima (pivote no se recorre)
+    for (int j = low; j <= high -1; j++){
+
+    	// si el elemento es menor o igual al pivote entonces...
+        if (arr[j] <= pivote){
+
+        	// incrementamos el valor de i
+            i++;
+
+            // intercambiamos las posiciones            
+            // posiciòn que guardamos antes // posiciòn en la que vamos
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    // todos los elementos hasta el arr[i] son menores o iguales al pivote
+    // hacemos el cambio de lugar del pivote a una posiciòn luego de estos elementos menores o =
+    swap(&arr[i + 1], &arr[high]);
+
+    // se devuelve la posiciòn del pivote
+    return (i + 1);
+}
+
+// se dividel el arreglo principal en subarreglos (valores menor y mayores al pivote) y se ordena c/u
+void quickSort(int arr[], int low, int high){
+
+    if (low < high){
+    	// posiciòn del pivote
+        int pivote = partir_arreglo(arr, low, high);
+
+      	// ordenar el arreglo con los datos menores al pivote
+        quickSort(arr, low, pivote - 1);
+
+        // ordenar el arreglo con los datos mayores al pivote
+        quickSort(arr, pivote + 1, high);
+    }
+}
+
+//-------------------------------------------------------
+
+int main(){
+	//std::cout<<"[hola]"<<std::endl;
+
+	const int SIZE_temp = 50; // tamaño predeterminado del arreglo
+	int arr[SIZE_temp];
+
+	int SIZE;
+	std::cout<<"Ingrese el tamaño del arreglo (n<50): "<<std::endl;
+	std::cin>> SIZE;
+
+    // Inicializa la semilla para numeros aleatorios
+    srand(static_cast<unsigned int>(time(0)));
+
+    // ------------------------------------------
+
+    // Llamar a la funcion para llenar el arreglo
+    llenarArreglo(arr, SIZE);
+
+    std::cout << "Arreglo de numeros enteros:\n";
+
+    // Llamar a la funcion para mostrar el arreglo
+    mostrarArreglo(arr, SIZE);
+	
+	std::cout << "\n";
+	//------------------------------------------------------------------
+
+    // INICIO EJECUCIÒN - Selecciòn
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    // aki llamar a la funciòn
+    metodoSeleccion(arr, SIZE);
+    // imprimir el arr arreglado tmb
+
+    // FIN EJECUCIÒN - Selecciòn
+    auto end = std::chrono::high_resolution_clock::now();
+
+    // Calcular duraciòn
+    std::chrono::duration<double> duration = (end-start)*1000;
+    std::cout<<"Tiempo ejecucion metodo SELECCION: "<< duration.count() << " milisegundos\n"<<std::endl;
+
+	std::cout << "Arreglo ordenado \n";
+    mostrarArreglo(arr, SIZE);
+	std::cout << "\n";
+   //-------------------------------------------------------------------
+    
+	// INICIO EJECUCIÒN - QuickSort
+	auto start_2 = std::chrono::high_resolution_clock::now();
+
+	// aki llamar a la funciòn
+	int high = SIZE-1;
+	int low = 0;
+	quickSort(arr, low, high);
+	// imprimir el arr arreglado tmb
+
+
+	// FIN EJECUCIÒN - QuickSort
+	auto end_2 = std::chrono::high_resolution_clock::now();
+
+	// Calcular duraciòn
+	std::chrono::duration<double> duration_2 = (end_2-start_2)*1000;
+	std::cout<<"Tiempo ejecucion metodo QUickSort: "<< duration_2.count() << " milisegundos\n"<<std::endl;
+
+	std::cout << "Arreglo ordenado \n";
+    mostrarArreglo(arr, SIZE);
+	
+	return 0;
+}
